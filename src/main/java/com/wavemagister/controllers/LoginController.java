69594ded66e6b080adc00d9ab0ebb1e2525d8fde
Stelios <Stelios@gmail.com>
2019-01-24 20:45:58 +0200

    Update LoginController.java

--- src/main/java/com/wavemagister/controllers/LoginController.java
+++ src/main/java/com/wavemagister/controllers/LoginController.java
@@ -3,33 +3,30 @@ package com.wavemagister.controllers;
 import com.wavemagister.entities.User;
 import com.wavemagister.dao.UserDAO;
 import com.wavemagister.entities.Login;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class LoginController
-{
+public class LoginController {
+
     @Autowired
     private UserDAO userDAO;
 
-    @RequestMapping(value = "/main", method=RequestMethod.POST)
+    @RequestMapping(value = "/main", method = RequestMethod.POST)
     public ModelAndView main(@ModelAttribute("login") Login login) {
         login.setUserDAO(userDAO);
 
         if (login.checkLogin()) {
             User user = Login.getLoggedInUser();
 
-            if (user.isActivated()){
-                switch(user.getRole()){
+            if (user.isActivated()) {
+                switch (user.getRole()) {
                     case "admin":
                         return new ModelAndView("redirect:/admin");
                     case "shipowner":
@@ -44,42 +41,57 @@ public class LoginController
         return new ModelAndView("redirect:/login");
     }
 
-    @RequestMapping( value = "/login")
+    @RequestMapping(value = "/login")
     public ModelAndView loginUser(@ModelAttribute("login") Login login, @ModelAttribute("register") User user) {
+
+        if (Login.getLoggedInUser() != null) {
+            switch (Login.getLoggedInUser().getRole()) {
+                case "admin":
+                    return new ModelAndView("redirect:/admin");
+                case "shipowner":
+                    return new ModelAndView("redirect:/shipowner");
+                case "charterer":
+                    return new ModelAndView("redirect:/charterer");
+            }
+        }
         return new ModelAndView("login");
     }
-    
-    @RequestMapping( value = "/logout")
+
+    @RequestMapping(value = "/logout")
     public ModelAndView logoutUser(@ModelAttribute("login") Login login, @ModelAttribute("register") User user) {
         Login.setLoggedInUser(null);
         Login.setLoggedIn(false);
         return new ModelAndView("redirect:/login");
     }
-    
-    @RequestMapping( value = "/change_password", method=RequestMethod.POST)
+
+    @RequestMapping(value = "/change_password", method = RequestMethod.POST)
     public ModelAndView changePassword(@RequestParam("password") String password) {
         Login.getLoggedInUser().setPassword(password);
         userDAO.updateUser(Login.getLoggedInUser());
         return new ModelAndView("redirect:/login");
     }
 
-    @RequestMapping( value = "/shipowner")
+    @RequestMapping(value = "/shipowner")
     public ModelAndView shipowner() {
-        if(!Login.isLoggedIn())
+        if (!Login.isLoggedIn()) {
             return new ModelAndView("redirect:/login");
-        if(!Login.getLoggedInUser().getRole().equals("shipowner"))
+        }
+        if (!Login.getLoggedInUser().getRole().equals("shipowner")) {
             return new ModelAndView("access_denied");
-            
+        }
+
         return new ModelAndView("shipowner");
     }
 
     @RequestMapping(value = "/charterer")
     public ModelAndView charterer() {
-        if(!Login.isLoggedIn())
+        if (!Login.isLoggedIn()) {
             return new ModelAndView("redirect:/login");
-        if(!Login.getLoggedInUser().getRole().equals("charterer"))
+        }
+        if (!Login.getLoggedInUser().getRole().equals("charterer")) {
             return new ModelAndView("access_denied");
-        
+        }
+
         return new ModelAndView("charterer");
     }
 }
